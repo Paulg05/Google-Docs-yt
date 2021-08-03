882033953335ef112fa035ed52a4c8249406cf2d
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-08-03 13:32:52 -0700

    Added: implemented firebase db and auth, using nextauth

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAK_hLS3vqlf9tZhQfJZsH6AbhpG6BW1R8",
+  authDomain: "docs-yt-23343.firebaseapp.com",
+  projectId: "docs-yt-23343",
+  storageBucket: "docs-yt-23343.appspot.com",
+  messagingSenderId: "827445679516",
+  appId: "1:827445679516:web:acf03984462bfb29a76459",
+};
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export { db };
