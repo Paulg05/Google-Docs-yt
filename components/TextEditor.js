e50d315500a5f1ddd5789bd7fc2ad4f83030abf0
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-08-04 16:07:48 -0700

    added header to the documents page

---
+++ components/TextEditor.js
@@ -0,0 +1,12 @@
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+
+function TextEditor() {
+  return (
+    <div>
+      <h1>Text Editor</h1>
+    </div>
+  )
+}
+
+export default TextEditor
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,69 @@ import Image from "next/image";
 import Header from "../components/Header";
 import { getSession, useSession } from "next-auth/client";
 import Login from "../components/Login";
+import Modal from "@material-tailwind/react/Modal";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+import { useState } from "react";
+import firebase from "firebase";
+import { db } from "../firebase";
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+import DocumentRow from "../components/DocumentRow";
 
 export default function Home() {
   //login logic
   const [session] = useSession();
 
   if (!session) return <Login />;
+
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
+  const [snapshot] = useCollectionOnce(
+    db
+      .collection("userDocs")
+      .doc(session.user.email)
+      .collection("docs")
+      .orderBy("timestamp", "desc")
+  );
+
+  const createDocument = () => {
+    if (!input) return;
+
+    db.collection("userDocs").doc(session.user.email).collection("docs").add({
+      fileName: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
+    setShowModal(false);
+  };
+
+  const modal = (
+    <Modal size="sm" active={showModal} toggler={() => setShowModal(false)}>
+      <ModalBody>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className="outline-none w-full"
+          placeholder="Enter name of document..."
+          onKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          color="blue"
+          buttonType="link"
+          onClick={(e) => setShowModal(false)}
+          ripple="dark"
+        >
+          Cancel
+        </Button>
+        <Button color="blue" onClick={createDocument} ripple="light">
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 
   return (
     <div>
@@ -19,6 +76,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      {modal}
       <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
           <div className="flex items-center justify-between py-6">
@@ -35,6 +93,7 @@ export default function Home() {
           </div>
           <div>
             <div
+              onClick={() => setShowModal(true)}
               className="relative h-52 w-40 border-2 
             cursor-pointer hover:border-blue-700"
             >
@@ -55,6 +114,15 @@ export default function Home() {
             <p className="mr-12">Date Created</p>
             <Icon name="folder" size="3xl" color="gray" />
           </div>
+
+          {snapshot?.docs.map((doc) => (
+            <DocumentRow
+              key={doc.id}
+              id={doc.id}
+              fil eName={doc.data().fileName}
+              date={doc.data().timestamp}
+            />
+          ))}
         </div>
       </section>
     </div>
